7098cd706202c6090f9ff875d929b33251e5bf4e
Sergey Sova <ssova@yandex-team.ru>
2017-11-24 12:33:42 +0300

    feat(create command): Change initial version from 0.0.1 to 0.0.0

--- scripts/create.component.js
+++ scripts/create.component.js
@@ -17,7 +17,7 @@ const mkdirp = pify(require('mkdirp'))
 const REJECTED_PACKAGES = ['core', 'theming', 'shadow']
 const AVAILABLE_BUNDLES = [{ name: 'core', checked: true }]
 const BASE_PACKAGE = 'Paper'
-const INITIAL_VERSION = '0.0.1'
+const INITIAL_VERSION = '0.0.0'
 const LIBRARY_NAME = require('../package.json').name
 
 
