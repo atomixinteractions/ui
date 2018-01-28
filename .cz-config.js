510b8571448426e48e9440a49802b61a221cbe28
Sergey Sova <mail@sergeysova.com>
2018-01-28 03:40:36 +0300

    chore: Add cz-customizable

---
+++ .cz-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  types: [
+    { value: 'feat', name: 'feat:     A new feature' },
+    { value: 'fix', name: 'fix:      A bug fix' },
+    { value: 'docs', name: 'docs:     Documentation only changes' },
+    { value: 'style', name: 'style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)' },
+    { value: 'refactor', name: 'refactor: A code change that neither fixes a bug nor adds a feature' },
+    { value: 'perf', name: 'perf:     A code change that improves performance' },
+    { value: 'build', name: 'build:    Changes that affect the build system or external dependencies' },
+    { value: 'test', name: 'test:     Adding missing tests' },
+    { value: 'chore', name: 'chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation' },
+    { value: 'revert', name: 'revert:   Revert to a commit' },
+    { value: 'wip', name: 'wip:      Work in progress' },
+  ],
+  scopes: [
+    { name: '@backend' },
+    { name: '@config' },
+    { name: '@frontend' },
+    { name: '@ui' },
+  ],
+  allowCustomScopes: true,
+  allowBreakingChanges: ['feat', 'fix', 'revert'],
+}
