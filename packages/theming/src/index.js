9d48b24158264d699a468924398947ab41d44630
Sergey Sova <mail@sergeysova.com>
2018-02-09 20:09:15 +0300

    docs(@ui/theming): add doc comments for functions

--- packages/theming/src/index.js
+++ packages/theming/src/index.js
@@ -1,12 +1,25 @@
+/* eslint-disable no-magic-numbers */
+export { defaultTheme } from './default-theme'
 
 /**
- *
+ * Get property from pallete
  * @param {string} name Name of the palette
- * @param {number} shade Shading for selected palette
+ * @param {number|string} [shade] Shading for selected palette
+ * @param {string} [defaultValue] Value return if color not found in palette
+ * @return {(props: any) => string}
  */
-export const palette = (name, shade = 0) => props =>
-  props.theme.palette[name][shade]
+export const palette = (name, shade = 500, defaultValue = 'black') => (props) => (
+  props.theme.palette[name] ? (props.theme.palette[name][shade] || defaultValue) : defaultValue
+)
 
-export const ifProp = (name, ifStyles, elseStyles = null) => props => props[name]
-  ? ifStyles
-  : elseStyles
+/**
+ * Apply styles if property found
+ * @param {string} name Property name to search for
+ * @param {object|string} stylesThen Styles applied if property found and truthy
+ * @param {object|string} [stylesElse] Styles applied if property not found or found but falsy
+ * @return {(props: any) => object|string}
+ */
+export const ifProp = (name, stylesThen, stylesElse = null) => (props) => props[name]
+  ? stylesThen
+  : stylesElse
+
