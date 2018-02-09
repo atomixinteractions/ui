bb6472fbc4b2f35fe734d0e348fdf2bfeb33548f
Sergey Sova <mail@sergeysova.com>
2018-02-09 20:12:57 +0300

    refactor(@ui/button): remove old styles. add primary, danger, warning
    Remove old "elegant" style in favor to Button.{Primary, Danger, Warning} and Button
    
    BREAKING CHANGE: Removed old props. Add new button subtypes

--- packages/button/src/index.js
+++ packages/button/src/index.js
@@ -1,55 +1,89 @@
-import styled, { css } from 'styled-components'
-import PropTypes from 'prop-types'
-
-import { shadows } from '@atomix-ui/shadow'
-import { ifProp } from '@atomix-ui/theming'
+/* eslint-disable no-magic-numbers */
+import styled from 'styled-components'
+import { palette } from '@atomix-ui/theming'
 
 
-export const Button = styled.div`
+/**
+ * Define basic button for all cases.
+ * Button don't have any custom properties.
+*/
+export const Button = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
-  text-transform: uppercase;
-  transition: all .23s ease-in-out;
+  /* transition: all .23s ease-in-out; */
   cursor: pointer;
   user-select: none;
-  color: black;
+  color: ${palette('gray', 'text')};
   box-sizing: border-box;
 
-  border-radius: 2px;
-  font-size: 14px;
-  letter-spacing: 0.56px;
-  height: 36px;
-  margin: 6px 8px;
-  min-width: 64px;
-  padding: 0 16px;
+  border: none;
+  border-radius: 3px;
+  font-size: 1.4rem;
+  letter-spacing: 0.5px;
+  line-height: 4rem;
+  margin: 0;
+  padding: 0 1rem;
   font-weight: 500;
 
-  background-color: rgba(0,0,0,.05);
-  box-shadow: ${shadows[1]};
+  background-color: ${palette('gray')};
 
   &:hover {
-    background-color: rgba(0,0,0,.07);
-    box-shadow: ${shadows[2]};
+    background-color: ${palette('gray', 600)};
   }
-  &:active {
-    box-shadow: ${shadows[6]};
+  &:active, &:focus {
+    outline: none;
+  }
+  &:focus:not(:active) {
+    outline: none;
+    /* box-shadow: 0 0 0 2px rgba(1, 82, 204, .3); */
   }
   &[disabled] {
-    box-shadow: none;
-    background-color: rgba(0,0,0,.05);
+    cursor: default;
+    background-color: ${palette('gray')};
     opacity: 88%;
-    color: rgba(0,0,0,.26);
+    color: ${palette('gray', 300)};
   }
 `
 
-Button.propTypes = {
-  disabled: PropTypes.bool,
-  elegant: PropTypes.bool
-}
 
-Button.defaultProps = {
-  disabled: false,
-  elegant: false
-}
+/**
+ * Component define main action in form/page/modal.
+ * Don't have custom properties too.
+ */
+Button.Primary = Button.extend`
+  background-color: ${palette('primary')};
+  color: ${palette('primary', 'text')};
+
+  &:hover:not(:disabled) {
+    background-color: ${palette('primary', 600)};
+  }
+  &:active:not(:disabled) {
+    background-color: ${palette('primary', 400)};
+  }
+`
+
+Button.Danger = Button.extend`
+  background-color: ${palette('danger')};
+  color: ${palette('danger', 'text')};
+
+  &:hover:not(:disabled) {
+    background-color: ${palette('danger', 600)};
+  }
+  &:active:not(:disabled) {
+    background-color: ${palette('danger', 400)};
+  }
+`
+
+Button.Warning = Button.extend`
+  background-color: ${palette('warning')};
+  color: ${palette('warning', 'text')};
+
+  &:hover:not(:disabled) {
+    background-color: ${palette('warning', 600)};
+  }
+  &:active:not(:disabled) {
+    background-color: ${palette('warning', 400)};
+  }
+`
 
--- packages/button/src/index.story.js
+++ packages/button/src/index.story.js
@@ -1,12 +1,51 @@
 import React from 'react'
+import styled from 'styled-components'
 import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
 
 import { Button } from './'
 
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+
+  & > * + * {
+    margin-left: 2rem;
+  }
+`
+
 storiesOf('core/Button', module)
-  .add('Without props', () => (
-    <div>
-      <Button>Simple button</Button>
-    </div>
+  .add('Default', () => (
+    <Wrapper>
+      <Button onClick={action('default')}>Simple button</Button>
+      <Button onClick={action('default')} disabled>Disabled button</Button>
+    </Wrapper>
+  ))
+  .add('Primary', () => (
+    <Wrapper>
+      <Button.Primary onClick={action('primary')}>Simple button</Button.Primary>
+      <Button.Primary onClick={action('primary')} disabled>Disabled button</Button.Primary>
+    </Wrapper>
+  ))
+  .add('Danger', () => (
+    <Wrapper>
+      <Button.Danger onClick={action('danger')}>Simple button</Button.Danger>
+      <Button.Danger onClick={action('danger')} disabled>Disabled button</Button.Danger>
+    </Wrapper>
+  ))
+  .add('Warning', () => (
+    <Wrapper>
+      <Button.Warning onClick={action('warning')}>Simple button</Button.Warning>
+      <Button.Warning onClick={action('warning')} disabled>Disabled button</Button.Warning>
+    </Wrapper>
+  ))
+  .add('(compare)', () => (
+    <Wrapper>
+      <Button onClick={action('default')}>Button</Button>
+      <Button.Primary onClick={action('primary')}>Button.Primary</Button.Primary>
+      <Button.Danger onClick={action('danger')}>Button.Danger</Button.Danger>
+      <Button.Warning onClick={action('warning')}>Button.Warning</Button.Warning>
+      <Button onClick={action('disabled')} disabled>Button disabled</Button>
+    </Wrapper>
   ))
